ce0e7c36c038ecc0bd40ac297c2eded3c4b6eae3
Lucas <lucas.hsiung@gmail.com>
2020-09-14 20:25:30 -0700

    stuff

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir -p ~/bin
+cp -r tmux-files/ ~/
+cp -r vim-files/ ~/
+
+echo "installing binaries in ~/bin: requires sudo perms"
+which apt >/dev/null 2>&1
+if [ $? -eq 0  ]
+then
+    # fd
+    sudo apt-get install fd-find
+    # ripgrep
+    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb; sudo dpkg -i ripgrep_11.0.2_amd64.deb
+else
+    echo "Not a debian distro: install binaries manually"
+fi
+
