3f5a7eacacb75e490a8427958b8848454c6e7ab9
Lucas <lucas.hsiung@gmail.com>
2020-09-14 20:32:01 -0700

    install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p ~/bin
-cp -r tmux-files/ ~/
-cp -r vim-files/ ~/
+cp -r tmux-files/.tmux* ~/
+cp -r vim-files/.vim* ~/
 
 echo "installing binaries in ~/bin: requires sudo perms"
 which apt >/dev/null 2>&1
